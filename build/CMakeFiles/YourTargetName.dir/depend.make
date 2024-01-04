7c93266e0e7b555c171055603b4674203bced715
PeterSalnikov <psalniko@sfu.ca>
2024-01-03 16:03:39 -0800

    balls are moving again

---
+++ build/CMakeFiles/YourTargetName.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for YourTargetName.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/balls.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/balls.dir/main.cpp.o"
+  "CMakeFiles/balls.dir/main.cpp.o.d"
+  "balls"
+  "balls.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/balls.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
